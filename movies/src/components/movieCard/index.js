d803b05373451fa65726909a2e8aba365d920651
YisiHuang <20095257@mail.wit.ie>
2023-11-26 21:53:27 +0000

    Fix code.

--- movies/src/components/movieCard/index.js
+++ movies/src/components/movieCard/index.js
@@ -17,7 +17,7 @@ import { Link } from "react-router-dom";
 import Avatar from '@mui/material/Avatar';
 
 export default function MovieCard({ movie, action }) { 
-  const { favorites, addToFavorites } = useContext(MoviesContext);
+  const { favorites } = useContext(MoviesContext);
   const isMovie = Boolean(movie.title);
 
   if (favorites.find((id) => id === movie.id)) {
